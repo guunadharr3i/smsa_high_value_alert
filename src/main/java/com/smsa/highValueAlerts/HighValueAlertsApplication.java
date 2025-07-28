8354193169efb3f99ec1d4dda0910ccc82f09900
Garikapati Gopi <gopi.g@eleserv.in>
2025-07-28 14:58:13 +0530

    jar to war

--- src/main/java/com/smsa/highValueAlerts/HighValueAlertsApplication.java
+++ src/main/java/com/smsa/highValueAlerts/HighValueAlertsApplication.java
@@ -2,9 +2,10 @@ package com.smsa.highValueAlerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HighValueAlertsApplication {
+public class HighValueAlertsApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HighValueAlertsApplication.class, args);
